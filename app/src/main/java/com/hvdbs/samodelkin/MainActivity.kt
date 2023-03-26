1766ca01a77c815b7cfe2c92e085505d082ae0e2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-03-26 11:53:24 +0300

    Easy android example

--- app/src/main/java/com/hvdbs/samodelkin/MainActivity.kt
+++ app/src/main/java/com/hvdbs/samodelkin/MainActivity.kt
@@ -2,10 +2,52 @@ package com.hvdbs.samodelkin
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+
+private const val CHARACTER_DATA_KEY = "CHARACTER_DATA_KEY"
+
+private var Bundle.characterData
+    get() = getSerializable(CHARACTER_DATA_KEY) as CharacterGenerator.CharacterData
+    set(value) = putSerializable(CHARACTER_DATA_KEY, value)
 
 class MainActivity : AppCompatActivity() {
+    private var characterData = CharacterGenerator.generate()
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.characterData = characterData
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        characterData = savedInstanceState?.characterData ?: CharacterGenerator.generate()
+
+        val nameTextView = findViewById<TextView>(R.id.nameTextView)
+        val raceTextView = findViewById<TextView>(R.id.raceTextView)
+        val dexterityTextView = findViewById<TextView>(R.id.dexterityTextView)
+        val wisdomTextView = findViewById<TextView>(R.id.wisdomTextView)
+        val strengthTextView = findViewById<TextView>(R.id.strengthTextView)
+        val generateButton = findViewById<TextView>(R.id.generateButton)
+        generateButton.setOnClickListener {
+            characterData = CharacterGenerator.generate()
+
+            characterData.run {
+                nameTextView.text = name
+                raceTextView.text = race
+                dexterityTextView.text = dex
+                wisdomTextView.text = wis
+                strengthTextView.text = str
+            }
+        }
+
+        characterData.run {
+            nameTextView.text = name
+            raceTextView.text = race
+            dexterityTextView.text = dex
+            wisdomTextView.text = wis
+            strengthTextView.text = str
+        }
     }
 }
